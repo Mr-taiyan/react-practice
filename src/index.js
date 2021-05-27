321ffaa41fbab7438caec24cb1caa9eb7080e19c
chen <cty9388@hotmail.com>
2021-05-27 11:04:49 +0800

    scroll stably with getSnapshotDidUpdate lifycycle function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Scroll extends Component {
+  constructor() {
+    super();
+    this.Ref = React.createRef();
+    this.state = {
+      msgs: [],
+    };
+  }
+  pushNewMsg() {
+    this.setState((prev) => {
+      return { msgs: [prev.msgs.length, ...prev.msgs] };
+    });
+  }
+  getSnapshotBeforeUpdate() {
+    return this.Ref.current.scrollHeight;
+  }
+  componentDidMount() {
+    console.log(this.Ref.current);
+    for (let i = 0; i < 20; i++) {
+      this.pushNewMsg();
+    }
+    this.interval = setInterval(() => {
+      if (this.state.msgs.length === 200) {
+        clearInterval(this.interval);
+        return;
+      }
+      this.pushNewMsg();
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  componentDidUpdate(prevProps, prevState, prevScrollHeight) {
+    const scrollTop = this.Ref.current.scrollTop;
+    if (scrollTop < 5) return;
+    this.Ref.current.scrollTop =
+      scrollTop + (this.Ref.current.scrollHeight - prevScrollHeight);
+  }
+  render() {
+    return (
+      <div ref={this.Ref} className="snapshot-sample">
+        {this.state.msgs.map((item) => (
+          <div>{item}</div>
+        ))}
+      </div>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Scroll />, document.getElementById("root"));
