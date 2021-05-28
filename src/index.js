a689636face75ef15568f3f75f326e1076ba09ca
chen <cty9388@hotmail.com>
2021-05-28 14:45:09 +0800

    finish chat function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,45 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+class MessageList extends PureComponent {
+  render() {
+    let { messages } = this.props;
+    return (
+      <ul>
+        {messages.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
 class SendImage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      inputMsg: "",
+    };
+  }
+  changeInput = (event) => {
+    this.setState({
+      inputMsg: event.target.value,
+    });
+  };
+  handleClick = () => {
+    let newMessage = [...this.state.messages, this.state.inputMsg];
+    this.setState({
+      messages: newMessage,
+      inputMsg: "",
+    });
+  };
   render() {
     return (
       <div>
-        <input />
-        <button>Send</button>
+        <MessageList messages={this.state.messages} />
+        <input value={this.state.inputMsg} onChange={this.changeInput} />
+        <button onClick={this.handleClick}>Send</button>
       </div>
     );
   }
