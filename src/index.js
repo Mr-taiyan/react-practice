b03369cdfdda942f1f75128dd8022a059d3196ee
chen <cty9388@hotmail.com>
2021-05-28 14:20:26 +0800

    add new index.css file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 ReactDOM.render(<div>test</div>, document.getElementById("root"));
