465713c005f98427e0e0fd5edf60b780854b329a
chen <cty9388@hotmail.com>
2021-06-07 17:52:16 +0800

    counter app with using react-redux

--- src/index.js
+++ src/index.js
@@ -1,56 +1,75 @@
 import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { connect } from "react-redux";
 import { bindActionCreators, combineReducers, createStore } from "redux";
 import "./index.css";
 
-function run() {
-  const initialState = { count: 0 };
+const initialState = { count: 0 };
 
-  const counter = (state = initialState, action) => {
-    switch (action.type) {
-      case "PLUS_ONE":
-        return { count: state.count + 1 };
-      case "MINUS_ONE":
-        return { count: state.count - 1 };
-      case "CUSTOM_COUNT":
-        return { count: state.count + action.payload.count };
-      default:
-        break;
-    }
-    return state;
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case "PLUS_ONE":
+      return { count: state.count + 1 };
+    case "MINUS_ONE":
+      return { count: state.count - 1 };
+    case "CUSTOM_COUNT":
+      return { count: state.count + action.payload.count };
+    default:
+      break;
+  }
+  return state;
+};
+
+const store = createStore(counter);
+
+function plusOne() {
+  return {
+    type: "PLUS_ONE",
   };
-
-  const todos = (state = {}) => state;
-
-  const store = createStore(
-    combineReducers({
-      todos,
-      counter,
-    })
-  );
-  function plusOne() {
-    return { type: "PLUS_ONE" };
-  }
-  function minusOne() {
-    return { type: "MINUS_ONE" };
-  }
-  function customCount(count) {
-    return { type: "CUSTOM_COUNT", payload: { count } };
-  }
-
-  plusOne = bindActionCreators(plusOne, store.dispatch);
-
-  store.subscribe(() => console.log(store.getState()));
-
-  plusOne();
-  store.dispatch(minusOne());
-  store.dispatch(customCount(5));
 }
 
-ReactDOM.render(
-  <div>
-    <button onClick={run}>run</button>
-    <p>查看运行结果</p>
-  </div>,
-  document.getElementById("root")
-);
+function minusOne() {
+  return {
+    type: "MINUS_ONE",
+  };
+}
+
+class Counter extends Component {
+  render() {
+    const { count, plusOne, minusOne } = this.props;
+    return (
+      <div className="counter">
+        <button onClick={minusOne}>-</button>
+        <span style={{ display: "inline-block", margin: "0 10px" }}>
+          {count}
+        </span>
+        <button onClick={plusOne}>+</button>
+      </div>
+    );
+  }
+}
+
+function mapStatetoProps(state) {
+  return {
+    count: state.count,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ plusOne, minusOne }, dispatch);
+}
+
+const ConnectedCounter = connect(mapStatetoProps, mapDispatchToProps)(Counter);
+
+class CounterExample extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedCounter />
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(<CounterExample />, document.getElementById("root"));
