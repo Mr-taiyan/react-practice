d14a7f4defcb59ed39ee0ab572c866443c99decc
chen <cty9388@hotmail.com>
2021-06-22 13:08:29 +0800

    add precondition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+function useSingletom() {
+  console.log("run");
+}
+
+const MyCompo = () => {
+  useSingletom();
+  const [num, setNum] = useState(0);
+  function handleClick() {
+    setNum((prev) => prev + 1);
+  }
+  return (
+    <div>
+      my component{num}
+      <button onClick={handleClick}>click</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<MyCompo></MyCompo>, document.getElementById("root"));
