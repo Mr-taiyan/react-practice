e987e6cbbf59c521d7d1d61f2df1fd65c4752614
chen <cty9388@hotmail.com>
2021-06-23 10:48:14 +0800

    precondition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+export function UserList() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchUsers = async () => {
+    console.log("fetch users");
+  };
+
+  return (
+    <div className="user-list">
+      <button onClick={fetchUsers} disabled={loading}>
+        {loading ? "Loading..." : "show users"}
+      </button>
+      {error && <div style={{ color: "red" }}>failed: {String(error)}</div>}
+      <br />
+      <ul>
+        {users &&
+          users.length > 0 &&
+          users.map((user) => {
+            return <li key={user.id}>{user.first_name}</li>;
+          })}
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<UserList />, document.getElementById("root"));
