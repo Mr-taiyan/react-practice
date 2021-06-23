ba5189c0937cac01ce01a2d41cbfd2d6eb3d980c
chen <cty9388@hotmail.com>
2021-06-23 10:34:34 +0800

    custom hook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useCallback, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+function useCount(initial) {
+  let [count, setCount] = useState(initial);
+  let increment = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, [setCount]);
+  let decrement = useCallback(() => {
+    setCount((prev) => prev - 1);
+  }, [setCount]);
+  let reset = useCallback(() => {
+    setCount(0);
+  }, [setCount]);
+  return { count, increment, decrement, reset };
+}
+
+function Example() {
+  const { count, increment, decrement, reset } = useCount(0);
+  return (
+    <div>
+      {count}
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Example />, document.getElementById("root"));
