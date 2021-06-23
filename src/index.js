adf279ed94164758782910c36ddbc9c687a57da0
chen <cty9388@hotmail.com>
2021-06-23 11:43:25 +0800

    precondition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useCallback } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+function ScrollTop() {
+  const goTop = useCallback(() => {
+    document.body.scrollTop = 0;
+  }, []);
+
+  const style = {
+    position: "fixed",
+    right: "10px",
+    bottom: "10px",
+  };
+  return (
+    <button onClick={goTop} style={style}>
+      Back to Top
+    </button>
+  );
+}
 
 ReactDOM.render(<div>test</div>, document.getElementById("root"));
