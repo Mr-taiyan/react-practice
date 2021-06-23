f74ebace0a35bb3c6f0b89704bd10d45776b21b1
chen <cty9388@hotmail.com>
2021-06-23 18:29:51 +0800

    use react router to receive params

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Link, Route, HashRouter as Router } from "react-router-dom";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 
 const Home = () => <h1>Home</h1>;
 const Hello = () => <h1>Hello</h1>;
 const About = () => <h1>About</h1>;
 
+const Topic = ({ match }) => {
+  return <h1>Topci {match.params.id}</h1>;
+};
 export default class RouterSample extends PureComponent {
   render() {
     return (
       <Router>
         <div>
           <ul id="menu">
-            <li>
+            {/* <li>
               <Link to="/home">home</Link>
             </li>
             <li>
@@ -21,13 +24,23 @@ export default class RouterSample extends PureComponent {
             </li>
             <li>
               <Link to="/about">about</Link>
+            </li> */}
+            <li>
+              <Link to="/topic/1">Topic 1</Link>
+            </li>
+            <li>
+              <Link to="/topic/2">Topic 2</Link>
+            </li>
+            <li>
+              <Link to="/topic/3">Topic 3</Link>
             </li>
           </ul>
         </div>
         <div id="page-container">
-          <Route path="/home" component={Home}></Route>
+          {/* <Route path="/home" component={Home}></Route>
           <Route path="/hello" component={Hello}></Route>
-          <Route path="/about" component={About}></Route>
+          <Route path="/about" component={About}></Route> */}
+          <Route path="/topic/:id" component={Topic}></Route>
         </div>
       </Router>
     );
