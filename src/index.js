4050dc77a8e05ae32fc080703ebb43269532de61
chen <cty9388@hotmail.com>
2021-06-23 11:34:11 +0800

    teachers useAsync

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent, useState } from "react";
+import React, { Component, PureComponent, useCallback, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
@@ -22,10 +22,39 @@ function useFetchUsers(url) {
   return { users, loading, error, fetchUsers };
 }
 
+function useAsync(asyncFunction) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const execute = useCallback(() => {
+    setData(null);
+    setLoading(true);
+    setError(null);
+    return asyncFunction()
+      .then((res) => {
+        setData(res);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [asyncFunction]);
+  return { execute, data, loading, error };
+}
+
 export function UserList() {
-  const { fetchUsers, loading, error, users } = useFetchUsers(
-    "https://reqres.in/api/users/"
-  );
+  const {
+    execute: fetchUsers,
+    data: users,
+    loading,
+    error,
+  } = useAsync(async () => {
+    const res = await fetch("https://reqres.in/api/users/");
+    const json = await res.json();
+    return json.data;
+  });
   return (
     <div className="user-list">
       <button onClick={fetchUsers} disabled={loading}>
