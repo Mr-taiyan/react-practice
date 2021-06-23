65662249ec478ce983580d34474e575f75d3ed28
chen <cty9388@hotmail.com>
2021-06-23 11:55:22 +0800

    useScroll

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React, { Component, PureComponent, useCallback } from "react";
+import React, {
+  Component,
+  PureComponent,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const getPosition = () => {
+  return {
+    x: document.body.scrollLeft,
+    y: document.body.scrollTop,
+  };
+};
+
+const useScroll = () => {
+  const [position, setPostion] = useState(getPosition());
+  useEffect(() => {
+    const handler = () => {
+      setPostion(getPosition());
+    };
+    document.addEventListener("scroll", handler);
+    return () => {
+      document.removeEventListener("scroll", handler);
+    };
+  }, []);
+  return position;
+};
+
 function ScrollTop() {
+  const { y } = useScroll();
   const goTop = useCallback(() => {
     document.body.scrollTop = 0;
   }, []);
@@ -12,11 +40,22 @@ function ScrollTop() {
     right: "10px",
     bottom: "10px",
   };
+  if (y > 300) {
+    return (
+      <button onClick={goTop} style={style}>
+        Back to Top
+      </button>
+    );
+  }
+  return null;
+}
+
+function Test() {
   return (
-    <button onClick={goTop} style={style}>
-      Back to Top
-    </button>
+    <div style={{ background: "yellow", height: "8000px" }}>
+      <ScrollTop />
+    </div>
   );
 }
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+ReactDOM.render(<Test />, document.getElementById("root"));
