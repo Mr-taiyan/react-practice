9ccec380e769fc4c391e5a6f1a4adb31f51def7c
chen <cty9388@hotmail.com>
2021-06-24 14:36:37 +0800

    my own state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useCallback, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+const App = () => {
+  const [data, setData] = useState({ price: 0, currency: "rmb" });
+  const handlePrice = useCallback(
+    (price) => {
+      setData({
+        ...data,
+        price,
+      });
+    },
+    [data]
+  );
+  const handleCurrency = useCallback(
+    (currency) => {
+      setData({
+        ...data,
+        currency,
+      });
+    },
+    [data]
+  );
+  return (
+    <div>
+      <input
+        value={data.price}
+        onChange={(evt) => {
+          handlePrice(evt.target.value);
+        }}
+      ></input>
+      <select
+        value={data.currency}
+        onChange={(evt) => {
+          handleCurrency(evt.target.value);
+        }}
+      >
+        <option value="rmb">RMB</option>
+        <option value="dollar">Dollar</option>
+      </select>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
