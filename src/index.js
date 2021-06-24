99f122c2e9648afab3f0edfc7bb85b641b8c6325
chen <cty9388@hotmail.com>
2021-06-24 11:06:47 +0800

    try using useSearchParam from react-use

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
 import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
+import { useSearchParam } from "react-use";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+const Demo = () => {
+  const edit = useSearchParam("edit");
+  return (
+    <div>
+      <div>edit: {edit || "🤷‍♂️"}</div>
+      <div>
+        <button
+          onClick={() =>
+            window.history.pushState(
+              {},
+              "",
+              window.location.pathname + "?edit=123"
+            )
+          }
+        >
+          Edit post 123 (?edit=123)
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() =>
+            window.history.pushState(
+              {},
+              "",
+              window.location.pathname + "?edit=999"
+            )
+          }
+        >
+          Edit post 999 (?edit=999)
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() =>
+            window.history.pushState({}, "", window.location.pathname)
+          }
+        >
+          Close modal
+        </button>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<Demo />, document.getElementById("root"));
