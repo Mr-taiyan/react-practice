1fa839942fef2661b806ab4223898d6e432e3c2b
chen <cty9388@hotmail.com>
2021-06-25 15:04:58 +0800

    update form to meet tutorial

--- src/index.js
+++ src/index.js
@@ -3,15 +3,51 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Form extends PureComponent {
-  handleSubmit = () => {
-    console.log("test");
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      error: null,
+    };
+  }
+  handleNameChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (!this.state.name) {
+      this.setState({ error: "name is required" });
+      return;
+    }
+    this.setState(
+      {
+        error: null,
+      },
+      () => {
+        console.log("form submit: ", this.state);
+      }
+    );
   };
   render() {
+    const { name } = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input id="name"></input>
-        <button>Submit</button>
+      <form className="comment-box" onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            value={name}
+            onChange={this.handleNameChange}
+          ></input>
+        </div>
+        {this.state.error && (
+          <div style={{ color: "red" }}>{this.state.error}</div>
+        )}
+        <div>
+          <button>Submit</button>
+        </div>
       </form>
     );
   }
