5597fc284486bc2053f9673a3aa4bcac228af67a
chen <cty9388@hotmail.com>
2021-06-25 14:52:43 +0800

    add a form

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+class Form extends PureComponent {
+  handleSubmit = () => {
+    console.log("test");
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input id="name"></input>
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<Form />, document.getElementById("root"));
