8e5843f075a7c84d3ca002f50ec0f284a9328613
chen <cty9388@hotmail.com>
2021-06-27 00:00:07 +0800

    update index.js to use useArticle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import useArticle from "./useArticle";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+const ArticleView = ({ id }) => {
+  const { data, loading, error } = useArticle(id);
+  if (error) return "Failed.";
+  if (!data || loading) return "loading...";
+
+  return (
+    <div className="exp-09-article-view">
+      <h1>
+        {id}. {data.title}
+      </h1>
+      <p>{data.content}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<ArticleView id={123} />, document.getElementById("root"));
