3f23632ab2f733ee377c58025ff5f6798efe0793
chen <cty9388@hotmail.com>
2021-06-27 00:09:23 +0800

    create useUser.js

---
+++ src/useUser.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import apiClient from "./apiClient";
+
+export default function useUser({ id }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+    setLoading(true);
+    setData(null);
+    setError(null);
+
+    apiClient
+      .get(`/users/${id}`)
+      .then((res) => {
+        setLoading(false);
+        setData(res.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err);
+      });
+  }, [id]);
+
+  return { data, loading, error };
+}
