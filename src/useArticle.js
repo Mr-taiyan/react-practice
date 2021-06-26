62899f554ba7a9128363d5dbc2c4d8a023c5d6cc
chen <cty9388@hotmail.com>
2021-06-26 23:39:13 +0800

    create useArticle.js

---
+++ src/useArticle.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import apiClient from "./apiClient";
+
+const useArticle = (id) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setData(null);
+    setError(null);
+
+    apiClient
+      .get(`/posts/${id}`)
+      .then((res) => {
+        setLoading(false);
+        setData(res.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err);
+      });
+  }, [id]);
+
+  return { data, loading, error };
+};
+
+export default useArticle;
