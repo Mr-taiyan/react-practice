7db627bb51528defc40582e82bd4d2754ca4375b
chen <cty9388@hotmail.com>
2021-06-27 12:37:25 +0800

    update index.js to follow boilerplate

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import useArticle from "./useArticle";
+import useComment from "./useComment";
+import useUser from "./useUser";
 
 const ArticleView = ({ id }) => {
-  const { data, loading, error } = useArticle(id);
+  const { data: article, loading, error } = useArticle(id);
+  const { data: comments } = useComment(id);
+  const { data: user } = useUser(article?.userId);
+
   if (error) return "Failed.";
-  if (!data || loading) return "loading...";
+  if (!article || loading) return "loading...";
 
   return (
     <div className="exp-09-article-view">
       <h1>
-        {id}. {data.title}
+        {id}. {article.title}
       </h1>
-      <p>{data.content}</p>
+      {user && (
+        <div className="user-info">
+          <img src={user.avatar} height="40px" alt="user"></img>
+          <div>{user.name}</div>
+          <div>{article.createdAt}</div>
+        </div>
+      )}
+      <p>{article.content}</p>
     </div>
   );
 };
