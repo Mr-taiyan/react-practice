c68fc1008ed97ff8c84ade151586b7550379c62f
chen <cty9388@hotmail.com>
2021-06-29 00:20:26 +0800

    add useKeyPress

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+const useKeyPress = (domNode = document.body) => {
+  const [key, setkey] = useState(null);
+  useEffect(() => {
+    const handleKeyPress = (evt) => {
+      setkey(evt.keyCode);
+    };
+    domNode.addEventListener("keypress", handleKeyPress);
+    return () => {
+      domNode.removeEventListener("keypress", handleKeyPress);
+    };
+  }, [domNode]);
+  return key;
+};
+
+function UseKeyPressExample() {
+  const key = useKeyPress();
+  return (
+    <div>
+      <h1>useKeyPress</h1>
+      <label>key press: {key || "N/A"}</label>
+    </div>
+  );
+}
+
+ReactDOM.render(<UseKeyPressExample />, document.getElementById("root"));
