add1cdaf55e0ba943900a53855b82269583043a3
chen <cty9388@hotmail.com>
2021-06-29 00:09:53 +0800

    simple event listener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+function ToggleButton({ value, onChange }) {
+  const handleClick = () => {
+    onChange(!value);
+  };
+  return <button onClick={handleClick}>{value ? "off" : "on"}</button>;
+}
+
+function ToggleButtonExample() {
+  const [on, setOn] = useState(true);
+  return (
+    <>
+      <h1>toggole button</h1>
+      <ToggleButton value={on} onChange={setOn}></ToggleButton>
+    </>
+  );
+}
+
+ReactDOM.render(<ToggleButton />, document.getElementById("root"));
