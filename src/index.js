da4d803047ed2c08231d4c0413a99eade1c9cdc9
chen <cty9388@hotmail.com>
2021-06-28 15:00:34 +0800

    try render props design pattern

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useCallback, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+function CounterRenderProps({ children }) {
+  const [count, setCount] = useState(0);
+  const increment = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+  const decrement = useCallback(() => {
+    setCount((prev) => prev - 1);
+  }, []);
+  return children({ increment, decrement, count });
+}
+
+function CounterRenderPropsExample() {
+  return (
+    <CounterRenderProps>
+      {({ count, increment, decrement }) => {
+        return (
+          <div>
+            <button onClick={decrement}>-</button>
+            <span>{count}</span>
+            <button onClick={increment}>+</button>
+          </div>
+        );
+      }}
+    </CounterRenderProps>
+  );
+}
+
+ReactDOM.render(<CounterRenderPropsExample />, document.getElementById("root"));
