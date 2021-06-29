bcec3392f6c40ea2013318161f65fe352d50150e
chen <cty9388@hotmail.com>
2021-06-29 18:42:10 +0800

    add useForm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useCallback, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+const useForm = (initialState = {}) => {
+  const [values, setValues] = useState(initialState);
+
+  const setFieldValue = useCallback((name, value) => {
+    setValues((values) => ({
+      ...values,
+      [name]: value,
+    }));
+  }, []);
+};
 
 ReactDOM.render(<div>test</div>, document.getElementById("root"));
