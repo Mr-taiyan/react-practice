4878f06650bb7155dbaa19fdc704eccbe4f6b4cc
chen <cty9388@hotmail.com>
2021-06-29 14:19:34 +0800

    add mouseTracker based on react documentation

--- src/index.js
+++ src/index.js
@@ -2,4 +2,30 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+class MouseTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: "100vh" }} onMouseMove={this.handleMouseMove}>
+        <h1>Move the mouse around!</h1>
+        <p>
+          The current mouse position is ({this.state.x}, {this.state.y})
+        </p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MouseTracker />, document.getElementById("root"));
