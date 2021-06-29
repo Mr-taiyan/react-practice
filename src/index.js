c41305336da589c2e318926191f34e60fcf437bd
chen <cty9388@hotmail.com>
2021-06-29 10:56:45 +0800

    add ChatRecipientPicker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+const ChatAPI = {
+  subscribeToFriendStatus(id, callback) {},
+  unsubscribeToFriendStatus(id, callback) {},
+};
+
+function useFriendStatus(friendId) {
+  const [isOnline, setIsOnline] = useState(null);
+
+  useEffect(() => {
+    function handleStatusChange(status) {
+      setIsOnline(status.isOnline);
+    }
+
+    ChatAPI.subscribeToFriendStatus(friendId);
+    return () => {
+      ChatAPI.unsubscribeToFriendStatus(friendId, handleStatusChange);
+    };
+  }, [friendId]);
+
+  return isOnline;
+}
+
+const friendList = [
+  { id: 1, name: "phoebe" },
+  { id: 2, name: "rachel" },
+  { id: 3, name: "ross" },
+];
+
+function Circle({ color }) {
+  return (
+    <div
+      style={{ width: "300px", height: "300px", background: `${color}` }}
+    ></div>
+  );
+}
+
+function ChatRecipientPicker() {
+  const [recipientId, setRecipientId] = useState(1);
+  const isRecipientOnline = useFriendStatus(recipientId);
+
+  return (
+    <>
+      <Circle color={isRecipientOnline ? "green" : "red"}></Circle>
+      <select
+        value={recipientId}
+        onChange={(e) => setRecipientId(Number(e.target.value))}
+      >
+        {friendList.map((friend) => (
+          <option key={friend.id} value={friend.id}>
+            {friend.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+}
+
+ReactDOM.render(<ChatRecipientPicker />, document.getElementById("root"));
