24888cfd6c2ccff9ba2a923b6abee279e871cc9f
chen <cty9388@hotmail.com>
2021-06-29 15:32:43 +0800

    add code base based on react documentation

--- src/index.js
+++ src/index.js
@@ -2,4 +2,45 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+const DataSourse = {
+  getComments() {},
+  addChangeListener(prop) {},
+  removeChangeListener(prop) {},
+};
+
+function Comment({ comment }) {
+  return <div>{comment}</div>;
+}
+
+class CommentList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      comments: DataSourse.getComments(),
+    };
+  }
+
+  componentDidMount() {
+    DataSourse.addChangeListener(this.handleChange);
+  }
+  componentWillUnmount() {
+    DataSourse.removeChangeListener(this.handleChange);
+  }
+  handleChange() {
+    this.setState({
+      comments: DataSourse.getComments(),
+    });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.comments.map((comment) => (
+          <Comment comment={comment}></Comment>
+        ))}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<CommentList />, document.getElementById("root"));
