e035c630b85916c99b7be3524980bd7f1f134e99
chen <cty9388@hotmail.com>
2021-06-29 15:55:58 +0800

    hoc usage

--- src/index.js
+++ src/index.js
@@ -76,4 +76,49 @@ class BlogPost extends React.Component {
   }
 }
 
+function withSubscription(WrappedComponent, selectData) {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {
+        // blogPost: DataSource.getBlogPost(props.id),
+        data: selectData(DataSource, props),
+      };
+    }
+
+    componentDidMount() {
+      DataSource.addChangeListener(this.handleChange);
+    }
+
+    componentWillUnmount() {
+      DataSource.removeChangeListener(this.handleChange);
+    }
+
+    handleChange() {
+      this.setState({
+        data: selectData(DataSource, this.props),
+      });
+    }
+
+    render() {
+      return <WrappedComponent data={this.state.data} />;
+    }
+  };
+}
+
+const CommentListWithSubscription = withSubscription(
+  CommentList,
+  (DataSource) => {
+    return DataSource.getComments();
+  }
+);
+
+const BlogPostWithSubscription = withSubscription(
+  BlogPost,
+  (DataSource, props) => {
+    return DataSource.getBlogPost(props.id);
+  }
+);
+
 ReactDOM.render(<CommentList />, document.getElementById("root"));
