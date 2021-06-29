0a12b807f733eeb7a982bc66204d85644a999d3f
chen <cty9388@hotmail.com>
2021-06-29 11:21:55 +0800

    my own useReducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, PureComponent, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+const useReducer = (reducer, initialState) => {
+  const [state, setState] = useState(initialState);
+
+  const dispatch = (action) => {
+    setState(reducer(state, action));
+  };
+  return [state, dispatch];
+};
 
 ReactDOM.render(<div>test</div>, document.getElementById("root"));
