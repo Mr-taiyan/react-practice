f86bc4b980e64bc519ca0aed13986d2d60c8aee0
chen <cty9388@hotmail.com>
2021-06-30 09:53:56 +0800

    my may to deal with forwardRef

--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const FancyButton = React.forwardRef((props, ref) => {
-  console.log(ref, "inner");
+// const FancyButton = React.forwardRef((props, ref) => {
+//   console.log(ref, "inner");
+//   return (
+//     <button ref={ref} className="FancyButton">
+//       {props.children}
+//     </button>
+//   );
+// });
+
+function FancyButton({ reference, children }) {
+  console.log(reference, "inner");
   return (
-    <button ref={ref} className="FancyButton">
-      {props.children}
+    <button ref={reference} className="FancyButton">
+      {children}
     </button>
   );
-});
+}
 
 const ref = React.createRef();
 console.log(ref, "outer");
 
 ReactDOM.render(
-  <FancyButton ref={ref}>Click me</FancyButton>,
+  <FancyButton reference={ref}>Click me</FancyButton>,
   document.getElementById("root")
 );
