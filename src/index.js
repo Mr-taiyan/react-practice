986e17934161f666df3e9d6a9c152a45610c9800
chen <cty9388@hotmail.com>
2021-06-30 10:58:50 +0800

    forward ref with HOC

--- src/index.js
+++ src/index.js
@@ -2,26 +2,42 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// const FancyButton = React.forwardRef((props, ref) => {
-//   console.log(ref, "inner");
-//   return (
-//     <button ref={ref} className="FancyButton">
-//       {props.children}
-//     </button>
-//   );
-// });
-
-function FancyButton({ reference, children }) {
-  console.log(reference, "inner");
+const FancyButton = React.forwardRef((props, ref) => {
   return (
-    <button ref={reference} className="FancyButton">
-      {children}
+    <button ref={ref} className="FancyButton">
+      {props.children}
     </button>
   );
+});
+
+// function FancyButton({ reference, children }) {
+//   console.log(reference, "inner");
+//   return (
+//     <button ref={reference} className="FancyButton">
+//       {children}
+//     </button>
+//   );
+// }
+
+function logProps(Component) {
+  class LogProps extends Component {
+    componentDidUpdate(prevProps) {
+      console.log("old props", prevProps);
+      console.log("new props", this.props);
+    }
+
+    render() {
+      const { reference, ...rest } = this.props;
+      return <Component {...rest} ref={reference} />;
+    }
+  }
+
+  return React.forwardRef((props, ref) => {
+    return <LogProps {...props} reference={ref} />;
+  });
 }
 
 const ref = React.createRef();
-console.log(ref, "outer");
 
 ReactDOM.render(
   <FancyButton reference={ref}>Click me</FancyButton>,
