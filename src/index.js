20f22ce0dccd574f6792b7eaa1838be5689d8589
chen <cty9388@hotmail.com>
2021-06-30 09:47:47 +0800

    add example on react documentation

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+const FancyButton = React.forwardRef((props, ref) => {
+  console.log(ref, "inner");
+  return (
+    <button ref={ref} className="FancyButton">
+      {props.children}
+    </button>
+  );
+});
+
+const ref = React.createRef();
+console.log(ref, "outer");
+
+ReactDOM.render(
+  <FancyButton ref={ref}>Click me</FancyButton>,
+  document.getElementById("root")
+);
