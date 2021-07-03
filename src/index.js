89c84d5f184a24475a7ee5e97156674dfa0f65df
chen <cty9388@hotmail.com>
2021-07-03 13:49:07 +0800

    create TabsPage.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import SamplePages from "./MyRouter";
-import App from "./NestedRouting";
+// import App from "./NestedRouting";
+import App from "./TabsPage";
+import "antd/dist/antd.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
