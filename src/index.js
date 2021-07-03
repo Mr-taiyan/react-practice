048882f859c7fe47f62cfb4e337e8e02ed876fec
chen <cty9388@hotmail.com>
2021-07-03 13:57:37 +0800

    use useParams

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-import React, { Component, PureComponent } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
-ReactDOM.render(<div>test</div>, document.getElementById("root"));
+function BlogPost() {
+  let param = useParams();
+  let { slug } = param;
+  console.log(param);
+  return <div>Now showing post {slug}</div>;
+}
+
+function HomePage() {
+  return <div>home</div>;
+}
+
+ReactDOM.render(
+  <Router>
+    <Link to="/">homepage</Link>
+    <Link to="/blog/test">slug</Link>
+    <Switch>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path="/blog/:slug">
+        <BlogPost />
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
