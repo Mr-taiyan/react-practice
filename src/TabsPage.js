92cdab8a044b94392d1c1209c98c2177a7149acf
chen <cty9388@hotmail.com>
2021-07-03 14:19:13 +0800

    fix bug: useParams can only be used in the wrap of Router

--- src/TabsPage.js
+++ src/TabsPage.js
@@ -13,7 +13,9 @@ import { useSearchParam } from "react-use";
 const { TabPane } = Tabs;
 
 const App = () => {
+  console.log("enter");
   const { activeTab = "users" } = useParams();
+  console.log("log1");
   const page = parseInt(useSearchParam("page"), 10) || 1;
   const history = useHistory();
   const handleTabChange = useCallback(
@@ -28,34 +30,32 @@ const App = () => {
     },
   };
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <div>
-            <h1>Tabs Page</h1>
-            <Tabs activeKey={activeTab} onChange={handleTabChange}>
-              <TabPane tab="Users" key="users">
-                <Table
-                  dataSource={data}
-                  columns={[
-                    { dataIndex: "name", title: "User Name" },
-                    { dataIndex: "city", title: "City" },
-                  ]}
-                  pagination={pagination}
-                />
-              </TabPane>
-              <TabPane tab="Jobs" key="jobs">
-                <Table
-                  dataSource={data}
-                  columns={[{ dataIndex: "job", title: "Job Title" }]}
-                  pagination={pagination}
-                />
-              </TabPane>
-            </Tabs>
-          </div>
-        </Route>
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/">
+        <div>
+          <h1>Tabs Page</h1>
+          <Tabs activeKey={activeTab} onChange={handleTabChange}>
+            <TabPane tab="Users" key="users">
+              <Table
+                dataSource={data}
+                columns={[
+                  { dataIndex: "name", title: "User Name" },
+                  { dataIndex: "city", title: "City" },
+                ]}
+                pagination={pagination}
+              />
+            </TabPane>
+            <TabPane tab="Jobs" key="jobs">
+              <Table
+                dataSource={data}
+                columns={[{ dataIndex: "job", title: "Job Title" }]}
+                pagination={pagination}
+              />
+            </TabPane>
+          </Tabs>
+        </div>
+      </Route>
+    </Switch>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import SamplePages from "./MyRouter";
 // import App from "./NestedRouting";
-import App from "./TabsPage";
+import TabsPage from "./TabsPage";
 import "antd/dist/antd.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <TabsPage></TabsPage>
+    </Router>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
