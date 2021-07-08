3666606834da27268c9a9ec72f3ce0129e2c5462
chen <cty9388@hotmail.com>
2021-07-08 10:11:01 +0800

    try keyBy of lodash

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React, { Component, PureComponent } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { byName } from "./keyBy";
+
+console.log(byName);
 
 ReactDOM.render(<div>test</div>, document.getElementById("root"));
