9ef9a96bcfa229298384e89e2521db7654aef920
chen <cty9388@hotmail.com>
2021-07-10 12:52:38 +0800

    create two pages

---
+++ src/ClassProfilePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class ClassProfilePage extends React.Component {
+  showMessage = () => {
+    alert("Followed " + this.props.user);
+  };
+
+  handleClick = () => {
+    setTimeout(this.showMessage, 3000);
+  };
+
+  render() {
+    return <button onClick={this.handleClick}>Follow</button>;
+  }
+}
---
+++ src/FunctionProfilePage.js
@@ -0,0 +1,11 @@
+export function FunctionProfilePage(props) {
+  const showMessage = () => {
+    alert("Followed " + props.user);
+  };
+
+  const handleClick = () => {
+    setTimeout(showMessage, 3000);
+  };
+
+  return <button onClick={handleClick}>Follow</button>;
+}
